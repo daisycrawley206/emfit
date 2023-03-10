f0f04e3d481aa3759b24ca1e8798f3164bc502b0
Daisy Crawley <daisy.crawley.12@ucl.ac.uk>
2023-03-10 10:17:45 +0000

    surrogate data plot script work

--- mAffectiveGoNogo/cocomp_gonogo_modelruns.m
+++ mAffectiveGoNogo/cocomp_gonogo_modelruns.m
@@ -2,7 +2,7 @@
 clear all; 
 % batchRunEMfit(modelClassToFit,pathToData,resultsDir,'key1','val1','key2','val2');
 
-Data=generateExampleDataset(50,'results');
+Data=generateExampleDataset_Q(50,'results');
 
 % only models 11 and 12 are set up to accomodate 4 sessions of data
 % (Nch=640)
@@ -10,5 +10,7 @@ modelsToFit = [11,12]; %% ses change and no change models
 models = modelList;
 models = models(modelsToFit);
 
-batchRunEMfit('mAffectiveGoNogo', Data, 'results', 'modelstofit', modelsToFit)
+batchRunEMfit('mAffectiveGoNogo', Data, 'results', 'modelstofit', modelsToFit, 'maxit',3)
 %%,'checkGradients',1,'emit',2) 
+
+% emit, you can tell it to only run a set number of loops
