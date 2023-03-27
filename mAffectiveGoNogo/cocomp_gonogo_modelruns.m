55695db9d5a39cdb7e0530749649859e90bb61c3
Daisy Crawley <daisy.crawley.12@ucl.ac.uk>
2023-03-27 22:32:33 +0100

    real data for models update

--- mAffectiveGoNogo/cocomp_gonogo_modelruns.m
+++ mAffectiveGoNogo/cocomp_gonogo_modelruns.m
@@ -1,8 +1,32 @@
 %% Models to Fit
-clear all; 
+% clear all; 
 % batchRunEMfit(modelClassToFit,pathToData,resultsDir,'key1','val1','key2','val2');
 
-Data=generateExampleDataset(50,'results');
+%% example data
+
+% Data=generateExampleDataset(50,'results');
+
+%% or real data
+
+clear all;
+
+T = readtable('gonogo_data_forModel_270323.csv');
+T.a = T.action+1;
+
+C = unique(T.subID);
+
+for sj = 1:length(C);
+    S(sj).ID = C{(sj),1};
+    S(sj).a = T.a(((sj-1)*720+1):(sj)*720)';
+    S(sj).r = T.reinforcement(((sj-1)*720+1):(sj)*720)';
+    S(sj).s = T.stim(((sj-1)*720+1):(sj)*720)';
+    S(sj).w = T.session(((sj-1)*720+1):(sj)*720)';
+    S(sj).Nch = length(S(sj).a);
+end
+
+Data=S;
+
+%% models
 
 % only models 11 and 12 are set up to accomodate 4 sessions of data
 % (Nch=640)
